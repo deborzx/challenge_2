50e8f98321231a642294241909cf9b5c5fb42bb1
Debora <debora.ramos.2@outlook.com>
2022-04-28 17:55:01 -0300

    iniciando

--- code/src/components/BestTransport.vue
+++ code/src/components/BestTransport.vue
@@ -2,12 +2,39 @@
   <div class="title">
     <b-navbar toggleable="lg" type="dark" variant="info">
       <b-navbar-brand class="ml-2">
+        <img class="img" src="./../assets/logo.png" width="50px">
         <b>{{ appName }}</b>
       </b-navbar-brand>
     </b-navbar>
-  </div>
 
+    <div class="container">
+      <p class="type-one">Como é o frete que voce precisa?</p>
+      <hr align = "left" width="50%"/>
+
+      <p class="type-two">Destino</p>
+        <select v-model="selected">
+          <option disabled value="">Escolha um item</option>
+          <option>Expresso Oriente</option>
+          <option>LevoJa</option>
+          <option>Rapido Jacutinga</option>
+        </select>
+
+      <p class="type-two">Peso</p>
+        <input type="number" v-model="rafa" placeholder="Insira aqui o peso da carga em Kg">
+    </div>
+
+    <div class="btn">      
+    <button class="ajust">Analisar</button>
+    </div>
+
+    <div class="container best-alternatives">
+      <p class="type-one">Estas são as melhores alternativas de frete que encontramos para você</p>
+      <hr align = "left" width="50%"/>
+    </div>
+  </div>
 </template>
+
+
 
 <script>
 import {
@@ -30,7 +57,7 @@ export default {
   created() {
     // Implemente aqui o GET dos dados da API REST
     // para que isso ocorra na inicialização da pagina
-    this.appName = 'Melhor Frete'
+    this.appName = 'MELHOR FRETE'
   },
   methods: {
     // Implemente aqui os metodos utilizados na pagina
@@ -43,10 +70,54 @@ export default {
 
 <style scoped>
 .title .navbar {
-  background-color: #00aca6 !important;
+  background-color: #93C47D !important;
+}
+
+.img{
+  margin-right: 20px;
 }
 
 .title .navbar-brand {
   margin-left: 20px;
 }
+
+.container{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+}
+
+.type-one{
+  margin-top: 24px;
+  font-size: 16px;
+  font-weight: bold;
+}
+
+.btn{
+  display: flex;
+  justify-content: center;
+}
+
+.ajust{
+  align: center;
+  margin-top: 24px;
+  background: #92C47D;
+  font-family: arial;
+  font-weight: bold;
+  border: solid 2px;
+  border-radius: 6px;
+  width: 130px;
+  height: 41px;
+
+}
+
+.type-two{
+  margin-top: 24px;
+  font-size: 24px;
+}
+
+.best-alternatives{
+  margin-top: 24px;
+}
+
 </style>
